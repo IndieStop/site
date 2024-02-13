b9e2b4273f73fbe39d339c356bea9ce383200c51
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-02-13 21:15:23 +0530

    Fixed Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: vercel(),
-	integrations: [tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [tailwind(), react()]
 });
